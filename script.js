2d03a480556af66f00c62173333a5f5b5e166305
myNamesDallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-17 22:14:34 -0400

    Started the getHumanChoice function

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const getComputerChoice = () => {
     const computerChoice = Math.floor(Math.random() * 3);
 
     switch(computerChoice) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors"
+        case 0: 
+            return "Rock";
+        case 1: 
+            return "Paper";
+        case 2: 
+            return "Scissors"
+    }
+    return
+}
+
+const getHumanChoice = (player) => {
+    const computerChoice = computerChoice();
+
+    if (player === computerChoice) {
+        alert(`It's a tie! Both chose ${player}`)
+
+        return
     }
 }
