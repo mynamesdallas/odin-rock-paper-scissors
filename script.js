9c42898847c6f0dd4e12c9fce077980135f7acbf
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-17 20:02:26 -0400

    Set up my html page to accept my script and wrote the computer choice function

---
+++ script.js
@@ -0,0 +1,9 @@
+const getComputerChoice = () => {
+    const computerChoice = Math.floor(Math.random() * 3);
+
+    switch(computerChoice) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors"
+    }
+}
