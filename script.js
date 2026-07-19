2c67a8baed053590059148117b7c886f67eebd99
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-18 20:41:38 -0400

    Added the getHumanChoice logic and score variables for both players

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+let humanScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
     const computerChoice = Math.floor(Math.random() * 3);
 
     switch(computerChoice) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors"
+        case 0: 
+            return "rock";
+        case 1: 
+            return "paper";
+        case 2: 
+            return "scissors"
     }
+    return computerChoice
+}
+
+const getHumanChoice = (playerChoice) => {
+    return playerChoice = prompt("Choose Rock, Paper, or Scissors");
 }
