e5153cf26d45bbe8457b0db99642fdc1f95f6217
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-18 22:40:58 -0400

    Added the playRound function and players score variables

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 const getComputerChoice = () => {
     const computerChoice = Math.floor(Math.random() * 3);
 
@@ -15,6 +12,34 @@ const getComputerChoice = () => {
     return computerChoice
 }
 
-const getHumanChoice = (playerChoice) => {
-    return playerChoice = prompt("Choose Rock, Paper, or Scissors");
+const getHumanChoice = (userChoice) => {
+    return userChoice = prompt("Choose Rock, Paper, or Scissors");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    const human = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+
+    if (human === computer) {
+        console.log(`It's a tie! You both chose ${humanChoice}`);
+        return;
+    }
+
+    if (human === "rock" && computer === "scissors" ||
+        human === "paper" && computer === "rock" ||
+        human === "scissors" & computer === "paper") {
+            humanScore++;
+            console.log(`You win: ${human} beats ${computer}`);
+        } else {
+            computerScore++;
+            console.log(`You lose: ${computer} beats ${human}`);
+        }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
