10e0c9a28254224dae23bdecdb418e644eb6d36d
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-19 20:09:15 -0400

    Added the play game function and moved and moved the playRound function into it along with score variables

--- script.js
+++ script.js
@@ -16,30 +16,38 @@ const getHumanChoice = (userChoice) => {
     return userChoice = prompt("Choose Rock, Paper, or Scissors");
 }
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-    const human = humanChoice.toLowerCase();
-    const computer = computerChoice.toLowerCase();
+    const playRound = (humanChoice, computerChoice) => {
+        const human = humanChoice.toLowerCase();
+        const computer = computerChoice.toLowerCase();
 
-    if (human === computer) {
-        console.log(`It's a tie! You both chose ${humanChoice}`);
-        return;
+        if (human === computer) {
+            console.log(`It's a tie! You both chose ${humanChoice}`);
+            return;
+        }
+
+        if (human === "rock" && computer === "scissors" ||
+            human === "paper" && computer === "rock" ||
+            human === "scissors" & computer === "paper") {
+                humanScore++;
+                console.log(`You win: ${human} beats ${computer}`);
+                alert(`You win: ${human} beats ${computer}`);
+            } else {
+                computerScore++;
+                console.log(`You lose: ${computer} beats ${human}`);
+                alert(`You lose: ${computer} beats ${human}`);
+            }
     }
 
-    if (human === "rock" && computer === "scissors" ||
-        human === "paper" && computer === "rock" ||
-        human === "scissors" & computer === "paper") {
-            humanScore++;
-            console.log(`You win: ${human} beats ${computer}`);
-        } else {
-            computerScore++;
-            console.log(`You lose: ${computer} beats ${human}`);
-        }
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame()
